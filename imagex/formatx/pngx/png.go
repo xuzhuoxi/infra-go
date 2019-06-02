922a9b882c97dd531299a0992aba50aa8ee8c678
xuzhuoxi <xuzhuoxi@gmail.com>
2019-06-02 16:57:13 +0800

    增加保存png的level支持

--- imagex/formatx/pngx/png.go
+++ imagex/formatx/pngx/png.go
@@ -16,10 +16,27 @@ func init() {
 	formatx.RegisterFormat(string(formatx.PNG), EncodePNG, DecodePNG)
 }
 
-func EncodePNG(w io.Writer, m image.Image, _ interface{}) error {
-	return png.Encode(w, m)
+func EncodePNG(w io.Writer, m image.Image, options interface{}) error {
+	if nil == options {
+		return encodePNG(w, m, png.DefaultCompression)
+	} else {
+		switch level := options.(type) {
+		case png.CompressionLevel:
+			return encodePNG(w, m, level)
+		case *png.CompressionLevel:
+			return encodePNG(w, m, *level)
+		default:
+			return encodePNG(w, m, png.DefaultCompression)
+		}
+	}
 }
 
 func DecodePNG(r io.Reader) (image.Image, error) {
 	return png.Decode(r)
+}
+
+func encodePNG(w io.Writer, m image.Image, level png.CompressionLevel) error {
+	var e png.Encoder
+	e.CompressionLevel = level
+	return e.Encode(w, m)
 }
