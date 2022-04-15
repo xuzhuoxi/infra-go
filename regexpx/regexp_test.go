df056954405a9d7975c6628279f1f8d90bbc8283
xuzhuoxi <xuzhuoxi@gmail.com>
2022-04-15 22:34:58 +0800

    1.增加部分正则支持。

--- regexpx/regexp_test.go
+++ regexpx/regexp_test.go
@@ -6,6 +6,22 @@ import (
 	"testing"
 )
 
+var (
+	zeroStrArr = []string{"0", "1", "00", "01", "0A"}
+)
+
+var (
+	digStrArr = []string{"0", "a"}
+)
+
+func TestZero(t *testing.T) {
+	testRegexp(t, Zero, zeroStrArr)
+}
+
+func TestDigital(t *testing.T) {
+	testRegexp(t, Digital, digStrArr)
+}
+
 func TestConst(t *testing.T) {
 	html := `<link rel="stylesheet" href="https://static.studygolang.com/cosmo_bootstrap.min.css">`
 	html2 := `<ul class="nav navbar-nav navbar-right" id="userbar"><li class="first"><a href="/account/register">注册</a></li><li class="last"><a href="/account/login">登录</a></li></ul>`
@@ -14,5 +30,15 @@ func TestConst(t *testing.T) {
 	fmt.Println("---")
 	chi := "你好"
 	fmt.Println(regexp.MatchString(Chinese, chi))
+}
 
+func testRegexp(t *testing.T, regStr string, data []string) {
+	reg, err := regexp.Compile(regStr)
+	if nil != err {
+		t.Fatal(err)
+		return
+	}
+	for _, str := range data {
+		fmt.Println(str, "\t:", reg.MatchString(str))
+	}
 }
