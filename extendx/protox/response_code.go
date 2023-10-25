5d41b2e2f904853a7d08908035fa22cb8f0e7cdb
xuzhuoxi <xuzhuoxi@gmail.com>
2023-10-25 14:37:33 +0800

    1. protox: rename file result.go to response_code.go

---
+++ extendx/protox/response_code.go
@@ -0,0 +1,25 @@
+package protox
+const (
+    // CodeSuc 成功
+    CodeSuc int32 = 0
+    // CodeProtoFail 协议错误-协议不存在
+    CodeProtoFail int32 = 1
+    // CodeArgs 参数错误
+    CodeArgs int32 = 2
+    // CodeInternal 服务器内部错误
+    CodeInternal int32 = 3
+    // CodeDbQuery 数据库执行错误
+    CodeDbQuery int32 = 4
+    // CodeTimeout 请求超时
+    CodeTimeout int32 = 5
+    // CodeRight 权限不足
+    CodeRight int32 = 6
+    // CodeStatus 状态不匹配
+    CodeStatus int32 = 7
+    // CodeRepeat 请求重复
+    CodeRepeat int32 = 8
+    // CodeFrequent 请求过于频繁
+    CodeFrequent int32 = 9
+    // CodeOther 其它错误
+    CodeOther int32 = 10
+)
