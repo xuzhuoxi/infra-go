4fcb598ea901cb94e02601bccf9bc3452f5b49bc
xuzhuoxi <mailxuzhuoxi@163.com>
2025-04-16 00:33:32 +0800

    1. mathx: 添加两个随机函数 1. mathx: Add two random functions

--- mathx/rand.go
+++ mathx/rand.go
@@ -7,9 +7,24 @@ package mathx
 import (
 	"math"
 	"math/rand"
+	"time"
 )
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
 
 // RandBool 随机bool
 func RandBool() bool {
 	return rand.Int63() > math.MaxInt32
+}
+
+// RandFloat64 任意范围的浮点数
+func RandFloat64(min float64, max float64) float64 {
+	return min + rand.Float64()*(max-min)
+}
+
+// RandFloat32 任意范围的浮点数
+func RandFloat32(min float32, max float32) float32 {
+	return min + rand.Float32()*(max-min)
 }
